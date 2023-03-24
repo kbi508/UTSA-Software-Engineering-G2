69bb5d1453224d636795b9643f54fc180bbb1129
Connor Acred <c.acred@yahoo.com>
2023-03-24 15:13:58 -0500

    Added sprt so there would be a scrollbar to test.

--- onestoppopshop/src/products.js
+++ onestoppopshop/src/products.js
@@ -2,6 +2,7 @@
 import _12ozCokeCan from './assets/12ozcokecan.jpeg'
 import _12ozAWRootbeerCan from './assets/12oza&wrootbeercan.jpeg'
 import _12ozDrPepperCan from './assets/12ozdrpeppercan.jpeg'
+import _12ozSpriteCan from './assets/12ozspritecan.jpg'
 
 export const PRODUCTS = [
     {
@@ -35,6 +36,19 @@ export const PRODUCTS = [
         productName: '12oz Dr.Pepper',
         price: 0.95,
         productImage: _12ozDrPepperCan,
+        descript: 'Aliquip sit minim fugiat id ut consectetur commodo eu esse cupidatat mollit. Officia elit consectetur elit ullamco incididunt nostrud eu proident amet dolore. Ullamco irure quis mollit laboris ad occaecat reprehenderit. Minim quis cillum in nostrud elit Lorem irure fugiat est quis. Fugiat aliquip ullamco do exercitation elit aute voluptate. Lorem consequat eu sint dolor aute deserunt id ut ea.',
+        star1Rate: 0,
+        star2Rate: 0,
+        star3Rate: 0,
+        star4Rate: 0,
+        star5Rate: 0,
+        tags: []
+    },
+    {
+        id: 4,
+        productName: '12oz Sprite',
+        price: 1.95,
+        productImage: _12ozSpriteCan,
         descript: 'Aliquip sit minim fugiat id ut consectetur commodo eu esse cupidatat mollit. Officia elit consectetur elit ullamco incididunt nostrud eu proident amet dolore. Ullamco irure quis mollit laboris ad occaecat reprehenderit. Minim quis cillum in nostrud elit Lorem irure fugiat est quis. Fugiat aliquip ullamco do exercitation elit aute voluptate. Lorem consequat eu sint dolor aute deserunt id ut ea.',
         star1Rate: 0,
         star2Rate: 0,
