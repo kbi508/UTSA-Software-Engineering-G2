5d7a8d9bbd2a2168595537e6b4dbb12ca593e5f5
Connor Acred <c.acred@yahoo.com>
2023-03-24 14:43:51 -0500

    Created pho products list for DB spoofing.

---
+++ onestoppopshop/src/products.js
@@ -0,0 +1,46 @@
+// This is spoofed data, later to be loaded into the DB
+import _12ozCokeCan from './assets/12ozcokecan.jpeg'
+import _12ozAWRootbeerCan from './assets/12oza&wrootbeercan.jpeg'
+import _12ozDrPepperCan from './assets/12ozdrpeppercan.jpeg'
+
+export const PRODUCTS = [
+    {
+        id: 1,
+        productName: '12oz Coca-cola',
+        price: 1.15,
+        productImage: _12ozCokeCan,
+        descript: 'Aliquip sit minim fugiat id ut consectetur commodo eu esse cupidatat mollit. Officia elit consectetur elit ullamco incididunt nostrud eu proident amet dolore. Ullamco irure quis mollit laboris ad occaecat reprehenderit. Minim quis cillum in nostrud elit Lorem irure fugiat est quis. Fugiat aliquip ullamco do exercitation elit aute voluptate. Lorem consequat eu sint dolor aute deserunt id ut ea.',
+        star1Rate: 0,
+        star2Rate: 0,
+        star3Rate: 0,
+        star4Rate: 0,
+        star5Rate: 0,
+        tags: []
+    },
+    {
+        id: 2,
+        productName: '12oz A&W Rootbeer',
+        price: 1.35,
+        productImage: _12ozAWRootbeerCan,
+        descript: 'Aliquip sit minim fugiat id ut consectetur commodo eu esse cupidatat mollit. Officia elit consectetur elit ullamco incididunt nostrud eu proident amet dolore. Ullamco irure quis mollit laboris ad occaecat reprehenderit. Minim quis cillum in nostrud elit Lorem irure fugiat est quis. Fugiat aliquip ullamco do exercitation elit aute voluptate. Lorem consequat eu sint dolor aute deserunt id ut ea.',
+        star1Rate: 0,
+        star2Rate: 0,
+        star3Rate: 0,
+        star4Rate: 0,
+        star5Rate: 0,
+        tags: []
+    },
+    {
+        id: 3,
+        productName: '12oz Dr.Pepper',
+        price: 0.95,
+        productImage: _12ozDrPepperCan,
+        descript: 'Aliquip sit minim fugiat id ut consectetur commodo eu esse cupidatat mollit. Officia elit consectetur elit ullamco incididunt nostrud eu proident amet dolore. Ullamco irure quis mollit laboris ad occaecat reprehenderit. Minim quis cillum in nostrud elit Lorem irure fugiat est quis. Fugiat aliquip ullamco do exercitation elit aute voluptate. Lorem consequat eu sint dolor aute deserunt id ut ea.',
+        star1Rate: 0,
+        star2Rate: 0,
+        star3Rate: 0,
+        star4Rate: 0,
+        star5Rate: 0,
+        tags: []
+    }
+]
