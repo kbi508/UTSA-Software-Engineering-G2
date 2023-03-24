30b4e6a78914fdff98354df2918aea98713bff98
Connor Acred <c.acred@yahoo.com>
2023-03-24 14:24:44 -0500

    Initialized pages components.

---
+++ onestoppopshop/src/pages/shop/shop.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+
+export const Shop = () => {
+  return (
+    <div className='shop'>
+      <div className='shoptitle'>
+        <h1>Welcome to the PopShop!</h1>
+      </div>
+      <div className='products'>
+        {/* Need tp change into using back-end once setup.*/}
+      </div>
+    </div>
+  )
+}
