aa9c165acb1fce04e146d10b549a4855b1244dcd
Connor Acred <c.acred@yahoo.com>
2023-03-24 15:14:15 -0500

    Added styling for shop component.

---
+++ onestoppopshop/src/pages/shop/product.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+
+export const Product = (props) => {
+  const {id, productName, price, productImage, descript, star1Rate, star2Rate, star3Rate, star4Rate, star5Rate, tags} = props.data
+  return (
+    <div className='product'>
+        <img src={productImage} alt='Product'/>
+        <div className='prod-desc'>
+            <p><b>{productName}</b></p>
+            <p>${price}</p>
+        </div>
+    </div>
+  )
+}
--- onestoppopshop/src/pages/shop/shop.jsx
+++ onestoppopshop/src/pages/shop/shop.jsx
@@ -1,13 +1,17 @@
 import React from 'react'
+import {PRODUCTS} from '../../products'
+import { Product } from './product'
+import './shop.css'
 
 export const Shop = () => {
   return (
     <div className='shop'>
-      <div className='shoptitle'>
+      <div className='shopTitle'>
         <h1>Welcome to the PopShop!</h1>
       </div>
       <div className='products'>
         {/* Need tp change into using back-end once setup.*/}
+        {PRODUCTS.map((product) => (<Product data={product}/>))}
       </div>
     </div>
   )
