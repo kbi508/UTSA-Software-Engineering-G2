5ba8920aaebff505ce728da92eea0634a951649c
kbi508 <99996743+kbi508@users.noreply.github.com>
2023-03-24 15:54:43 -0500

    Added fanta

--- onestoppopshop/src/products.js
+++ onestoppopshop/src/products.js
@@ -3,6 +3,7 @@ import _12ozCokeCan from './assets/12ozcokecan.jpeg'
 import _12ozAWRootbeerCan from './assets/12oza&wrootbeercan.jpeg'
 import _12ozDrPepperCan from './assets/12ozdrpeppercan.jpeg'
 import _12ozSpriteCan from './assets/12ozspritecan.jpg'
+import _12ozFantaCan from './assets/12ozfantacan.webp'
 
 export const PRODUCTS = [
     {
@@ -49,6 +50,20 @@ export const PRODUCTS = [
         productName: '12oz Sprite',
         price: 1.95,
         productImage: _12ozSpriteCan,
+        descript: 'Aliquip sit minim fugiat id ut consectetur commodo eu esse cupidatat mollit. Officia elit consectetur elit ullamco incididunt nostrud eu proident amet dolore. Ullamco irure quis mollit laboris ad occaecat reprehenderit. Minim quis cillum in nostrud elit Lorem irure fugiat est quis. Fugiat aliquip ullamco do exercitation elit aute voluptate. Lorem consequat eu sint dolor aute deserunt id ut ea.',
+        star1Rate: 0,
+        star2Rate: 0,
+        star3Rate: 0,
+        star4Rate: 0,
+        star5Rate: 0,
+        tags: []
+    },
+
+    {
+        id: 5,
+        productName: '12oz Fanta',
+        price: 3.50,
+        productImage: _12ozFantaCan,
         descript: 'Aliquip sit minim fugiat id ut consectetur commodo eu esse cupidatat mollit. Officia elit consectetur elit ullamco incididunt nostrud eu proident amet dolore. Ullamco irure quis mollit laboris ad occaecat reprehenderit. Minim quis cillum in nostrud elit Lorem irure fugiat est quis. Fugiat aliquip ullamco do exercitation elit aute voluptate. Lorem consequat eu sint dolor aute deserunt id ut ea.',
         star1Rate: 0,
         star2Rate: 0,
