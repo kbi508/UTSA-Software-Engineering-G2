0a72d72e033aa2dfc15c24b91fd7809f613c9157
Connor Acred <c.acred@yahoo.com>
2023-03-26 16:44:38 -0500

    Added context to keep track of cart items

--- onestoppopshop/src/pages/shop/product.jsx
+++ onestoppopshop/src/pages/shop/product.jsx
@@ -1,7 +1,11 @@
-import React from 'react'
+import React, { useContext } from 'react'
+import {ShopContext} from '../../context/shop-context'
 
 export const Product = (props) => {
   const {id, productName, price, productImage, descript, star1Rate, star2Rate, star3Rate, star4Rate, star5Rate, tags} = props.data
+  const { addToCart, cartItems } = useContext(ShopContext)
+
+  const cartItemsAmount = cartItems[id]
   return (
     <div className='product'>
         <img src={productImage} alt='Product'/>
@@ -9,7 +13,9 @@ export const Product = (props) => {
             <p><b>{productName}</b></p>
             <p>${price}</p>
         </div>
-        <button className='addToCartBttn'>Add To Cart</button>
+        <button className='addToCartBttn' onClick={() => addToCart(id)}>
+          Add To Cart {cartItemsAmount > 0 && <>({cartItemsAmount})</>}
+        </button>
     </div>
   )
 }
