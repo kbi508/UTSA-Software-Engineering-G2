26b64706c9ee7150abfe981e58fd986cd7a491f9
kbi508 <99996743+kbi508@users.noreply.github.com>
2023-03-29 14:12:49 -0500

    added all images of products

--- onestoppopshop/src/products.js
+++ onestoppopshop/src/products.js
@@ -4,7 +4,15 @@ import _12ozAWRootbeerCan from './assets/12oza&wrootbeercan.jpeg'
 import _12ozDrPepperCan from './assets/12ozdrpeppercan.jpeg'
 import _12ozSpriteCan from './assets/12ozspritecan.jpg'
 import _12ozFantaCan from './assets/12ozfantacan.webp'
-import _12ozBigRedCan from './assets/12ozbigredcan.webp'
+import _12ozBigRedCan from './assets/12ozbigredcan.jpg'
+import _12ozMountainDewCan from './assets/12ozmountaindewcan.jpeg'
+import _12ozCrushGrapeCan from './assets/12ozcrushgrapecan.webp'
+import _20ozPepsiBottle from './assets/20ozpepsibottle.webp'
+import _12ozSierraMistCan from './assets/12ozsierramistcan.jpeg'
+
+
+
+
 
 export const PRODUCTS = [
     {
@@ -20,7 +28,7 @@ export const PRODUCTS = [
         star3Rate: 0,
         star4Rate: 0,
         star5Rate: 0,
-        tags: []
+        tags: ["#vintage", "#cola", "#good", "#soda"]
     },
     {
         id: 2,
@@ -85,9 +93,71 @@ export const PRODUCTS = [
     {
         id: 6,
         productName: '12oz Big Red',
-        price: 3.50,
+        price: 1.50,
         productImage: _12ozBigRedCan,
         weightAmount: 12,
+        weightType: 'oz',
+        descript: 'Aliquip sit minim fugiat id ut consectetur commodo eu esse cupidatat mollit. Officia elit consectetur elit ullamco incididunt nostrud eu proident amet dolore. Ullamco irure quis mollit laboris ad occaecat reprehenderit. Minim quis cillum in nostrud elit Lorem irure fugiat est quis. Fugiat aliquip ullamco do exercitation elit aute voluptate. Lorem consequat eu sint dolor aute deserunt id ut ea.',
+        star1Rate: 0,
+        star2Rate: 0,
+        star3Rate: 0,
+        star4Rate: 0,
+        star5Rate: 0,
+        tags: []
+    },
+
+    {
+        id: 7,
+        productName: '12oz Mountain Dew',
+        price: 2.75,
+        productImage: _12ozMountainDewCan,
+        weightAmount: 12,
+        weightType: 'oz',
+        descript: 'Aliquip sit minim fugiat id ut consectetur commodo eu esse cupidatat mollit. Officia elit consectetur elit ullamco incididunt nostrud eu proident amet dolore. Ullamco irure quis mollit laboris ad occaecat reprehenderit. Minim quis cillum in nostrud elit Lorem irure fugiat est quis. Fugiat aliquip ullamco do exercitation elit aute voluptate. Lorem consequat eu sint dolor aute deserunt id ut ea.',
+        star1Rate: 0,
+        star2Rate: 0,
+        star3Rate: 0,
+        star4Rate: 0,
+        star5Rate: 0,
+        tags: []
+    },
+
+    {
+        id: 8,
+        productName: '12oz Crush Grape',
+        price: 1.32,
+        productImage: _12ozCrushGrapeCan,
+        weightAmount: 12,
+        weightType: 'oz',
+        descript: 'Aliquip sit minim fugiat id ut consectetur commodo eu esse cupidatat mollit. Officia elit consectetur elit ullamco incididunt nostrud eu proident amet dolore. Ullamco irure quis mollit laboris ad occaecat reprehenderit. Minim quis cillum in nostrud elit Lorem irure fugiat est quis. Fugiat aliquip ullamco do exercitation elit aute voluptate. Lorem consequat eu sint dolor aute deserunt id ut ea.',
+        star1Rate: 0,
+        star2Rate: 0,
+        star3Rate: 0,
+        star4Rate: 0,
+        star5Rate: 0,
+        tags: []
+    },
+    {
+        id: 9,
+        productName: '20oz Pepsi',
+        price: 2.43,
+        productImage: _20ozPepsiBottle,
+        weightAmount: 20,
+        weightType: 'oz',
+        descript: 'Aliquip sit minim fugiat id ut consectetur commodo eu esse cupidatat mollit. Officia elit consectetur elit ullamco incididunt nostrud eu proident amet dolore. Ullamco irure quis mollit laboris ad occaecat reprehenderit. Minim quis cillum in nostrud elit Lorem irure fugiat est quis. Fugiat aliquip ullamco do exercitation elit aute voluptate. Lorem consequat eu sint dolor aute deserunt id ut ea.',
+        star1Rate: 0,
+        star2Rate: 0,
+        star3Rate: 0,
+        star4Rate: 0,
+        star5Rate: 0,
+        tags: []
+    },
+    {
+        id: 10,
+        productName: '12oz Sierra Mist',
+        price: 2.43,
+        productImage: _12ozSierraMistCan,
+        weightAmount: 20,
         weightType: 'oz',
         descript: 'Aliquip sit minim fugiat id ut consectetur commodo eu esse cupidatat mollit. Officia elit consectetur elit ullamco incididunt nostrud eu proident amet dolore. Ullamco irure quis mollit laboris ad occaecat reprehenderit. Minim quis cillum in nostrud elit Lorem irure fugiat est quis. Fugiat aliquip ullamco do exercitation elit aute voluptate. Lorem consequat eu sint dolor aute deserunt id ut ea.',
         star1Rate: 0,
