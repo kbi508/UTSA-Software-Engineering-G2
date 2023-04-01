1fee2cb07f562f806325c9c7466f1f8b915ae4eb
Connor Acred <c.acred@yahoo.com>
2023-04-01 17:32:40 -0500

    Delt with scrollbar issue.

--- onestoppopshop/src/pages/shop/shop.jsx
+++ onestoppopshop/src/pages/shop/shop.jsx
@@ -1,11 +1,19 @@
-import React from 'react'
+import React, { useEffect } from 'react'
 import {PRODUCTS} from '../../products'
 import { Product } from './product'
 import { Cart } from '../../components/cart'
 import { ShopContext } from '../../context/shop-context'
 import styles from './shop.module.css'
 
+
 export const Shop = () => {
+  // useEffect(() => {
+  //   document.body.style.overflowY = 'hidden'
+
+  //   return () => {
+  //     document.body.style.overflowY = 'auto'
+  //   }
+  // }, [])
 
   return (
     <div className={styles.shop}>
