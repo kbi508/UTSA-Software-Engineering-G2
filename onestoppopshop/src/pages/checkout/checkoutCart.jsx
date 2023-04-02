fa102f934af8afa6c692373ae31b29b0fef2b1fd
Connor Acred <c.acred@yahoo.com>
2023-04-02 01:52:09 -0500

    Added cancel button to checkout.

--- onestoppopshop/src/pages/checkout/checkoutCart.jsx
+++ onestoppopshop/src/pages/checkout/checkoutCart.jsx
@@ -3,17 +3,18 @@ import { PRODUCTS } from '../../products'
 import { CheckoutCartItem } from './checkoutCartItem'
 import {ShopContext} from '../../context/shop-context'
 import styles from './checkoutCart.module.css'
+import { useNavigate } from 'react-router-dom'
 
 export const CheckoutCart = (props) => {
   const { cartItems, getTotalCartAmount, toggleOpen } = useContext(ShopContext)
   const totalAmount = getTotalCartAmount().toFixed(2)
+  const navigator = useNavigate()
 
   const [tempCred, setTempCred] = useState(100)
 
   const chargeCredit = (amount) => {
 
   }
-
 
   return (
     <div className={styles.checkoutCart}>
@@ -29,7 +30,7 @@ export const CheckoutCart = (props) => {
             <p>Subtotal: ${Number(totalAmount).toFixed(2)}</p>
             <p>Credit: ${Number(tempCred).toFixed(2)} - ${Number(totalAmount).toFixed(2)} = {Number(tempCred-totalAmount).toFixed(2)}</p>
             <button className={`${styles.orderBttn} ${styles.cartBttn}`} > Place Order </button>
-            {/* <button className={`${styles.cancelBttn} ${styles.cartBttn}`}> Cancel </button> */}
+            <button className={`${styles.cancelBttn} ${styles.cartBttn}`} onClick={() => navigator('/')}> Cancel </button>
         </div>
     </div>
   )
