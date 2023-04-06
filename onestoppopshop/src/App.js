f6c68378c760e9cfe314d57bd8f8650b517e345e
Connor <c.acred@yahoo.com>
2023-04-06 12:39:30 -0500

    Added admin route.

--- onestoppopshop/src/App.js
+++ onestoppopshop/src/App.js
@@ -4,6 +4,7 @@ import { Navbar } from "./components/navbar"
 import { Shop } from './pages/shop/shop'
 import { Checkout } from './pages/checkout/checkout'
 import { Account } from './pages/account/account'
+import { Admin } from './pages/admin/admin'
 import { ShopContextProvider } from './context/shop-context';
 
 function App() {
@@ -16,6 +17,7 @@ function App() {
             <Route path="/" element={<Shop />} />
             <Route path="/checkout" element={<Checkout />} />
             <Route path="/account" element={<Account />} />
+            <Route path="/admin" element={<Admin />} />
           </Routes>
         </Router>
       </ShopContextProvider>
