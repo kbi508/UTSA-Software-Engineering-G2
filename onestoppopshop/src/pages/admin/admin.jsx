fad78868d56cd2c0142b3f0266489041fe4b4f06
Connor <c.acred@yahoo.com>
2023-04-06 13:03:58 -0500

    Initialized admin item component.

--- onestoppopshop/src/pages/admin/admin.jsx
+++ onestoppopshop/src/pages/admin/admin.jsx
@@ -1,12 +1,19 @@
 import React from 'react'
 import styles from './admin.module.css'
+import { ORDERS, USERS, CODES } from './spoof'
+import { PRODUCTS } from '../../products'
+import { AdminItem } from './adminItem'
 
 export const Admin = () => {
   return (
     <div className={styles.adminPage}>
         <div>
             <div className={styles.curOrders}>
-
+                {ORDERS.map((order) => {
+                    if (order.active)
+                        return <AdminItem data = {order}/>
+                    return <></>
+                })}
             </div>
             <div className={styles.pastOrders}>
 
@@ -18,7 +25,11 @@ export const Admin = () => {
         </div>
 
         <div className={styles.items}>
-            
+
+        </div>
+
+        <div className={styles.discountCodes}>
+
         </div>
     </div>
   )
---
+++ onestoppopshop/src/pages/admin/adminItem.jsx
@@ -0,0 +1,8 @@
+import React from 'react'
+import styles from './adminItem.module.css'
+
+export const AdminItem = () => {
+  return (
+    <div>AdminItem</div>
+  )
+}
