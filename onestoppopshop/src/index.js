413026c3448a92837d13bd033d8aee85d9e0b1e8
kbi508 <99996743+kbi508@users.noreply.github.com>
2023-04-10 15:45:01 -0500

    Nothing

--- onestoppopshop/src/index.js
+++ onestoppopshop/src/index.js
@@ -1,8 +1,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
-import { initializeApp } from "firebase/app";
-import { getDatabase, ref, set, push } from "firebase/database";
+// import { initializeApp } from "firebase/app";
+// import { getDatabase, ref, set, push } from "firebase/database";
 
 
 
@@ -13,36 +13,36 @@ root.render(
   </React.StrictMode>
 );
 
-const firebaseConfig = {
-  databaseURL: "https://onestoppopshop-cffc9-default-rtdb.firebaseio.com",
-};
+// const firebaseConfig = {
+//   databaseURL: "https://onestoppopshop-cffc9-default-rtdb.firebaseio.com",
+// };
 
-// Initialize Firebase
-const app = initializeApp(firebaseConfig);
+// // Initialize Firebase
+// const app = initializeApp(firebaseConfig);
 
 
 // Initialize Realtime Database and get a reference to the service
-function writeProductData(prodId, name, price, imageUrl, weightAmount, weightType, descript, star1Rate, star2Rate, star3Rate, star4Rate, star5Rate, quantity) {
-  const database = getDatabase(app);
-  const reference = ref(database, 'products/' + prodId);
+// function writeProductData(prodId, name, price, imageUrl, weightAmount, weightType, descript, star1Rate, star2Rate, star3Rate, star4Rate, star5Rate, quantity) {
+//   const database = getDatabase(app);
+//   const reference = ref(database, 'products/' + prodId);
 
 
-  set(reference, {
-    name: name,
-    price: price,
-    product_Image : imageUrl,
-    weight_Amount : weightAmount,
-    weight_Type : weightType,
-    prod_description: descript,
-    num_1_Stars: star1Rate,
-    num_2_Stars: star2Rate,
-    num_3_Stars: star3Rate,
-    num_4_Stars: star4Rate,
-    num_5_Stars: star5Rate,
-    quantity
+//   set(reference, {
+//     name: name,
+//     price: price,
+//     product_Image : imageUrl,
+//     weight_Amount : weightAmount,
+//     weight_Type : weightType,
+//     prod_description: descript,
+//     num_1_Stars: star1Rate,
+//     num_2_Stars: star2Rate,
+//     num_3_Stars: star3Rate,
+//     num_4_Stars: star4Rate,
+//     num_5_Stars: star5Rate,
+//     quantity
 
-  })
-}
+//   })
+// }
 // function populateFirebaseDatabase(jsonObjects) {
 //   const database = getDatabase(app);
 //   jsonObjects.forEach((jsonObject) => {
@@ -50,6 +50,6 @@ function writeProductData(prodId, name, price, imageUrl, weightAmount, weightTyp
 //   });
 // }
 //populateFirebaseDatabase(PRODUCTS);
-writeProductData("1", "12oz Coca-cola", "1.15", "./assets/12ozcokecan.jpeg", "12", "oz", "something else", 1, 2, 3, 4, 5, 7);
+//writeProductData("1", "12oz Coca-cola", "1.15", "./assets/12ozcokecan.jpeg", "12", "oz", "something else", 1, 2, 3, 4, 5, 7);
 //writeUserData("testtwo", "t2", "testtwo@gmail.com", "./assets/12ozbigredcan.jpg");
 
