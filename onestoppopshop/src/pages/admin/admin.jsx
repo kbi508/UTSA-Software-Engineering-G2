badb18ddde68d04a10a316b2b4f636a1ae2c544e
Connor Acred <c.acred@yahoo.com>
2023-04-17 18:25:25 -0500

    Now sorting can be either ascending or descending for orders on the admin panel.

--- onestoppopshop/src/pages/admin/admin.jsx
+++ onestoppopshop/src/pages/admin/admin.jsx
@@ -34,6 +34,10 @@ export const Admin = () => {
   
   const [curProdNum, setCurProdNum] = useState(null)
 
+  // How to sort orders:
+  const [orderSort, setOrderSort] = useState(null)
+  const [ascending, setAscending] = useState(false)
+
   // For new codes:
   const [codeText, setCodeText] = useState('')
   const [codeNum, setCodeNum] = useState(null)
@@ -43,6 +47,66 @@ export const Admin = () => {
         setSelected(null)
     else
         setSelected(selUid)
+  }
+
+  const fetchOrders = async () => {
+    try {
+        const ordersRef = ref(database, 'orders')
+        const snapshot = await get(ordersRef)
+        if (snapshot.exists()) {
+            const data = snapshot.val()
+            const orderNumbers = Object.keys(data)
+            const finalOrders = Object.values(data)
+            finalOrders.forEach((order, index) => {
+                order.key = orderNumbers[index]
+            })
+            // Remove empty items in finalOrders (possibly weird firebase thing)
+            // Firebase makes the items into a list/array rather than a JSON object, this converts it back:
+            // Note that this only happens for SOME items, some are read as JSONs as intended, and thus don't need conversion.
+            finalOrders.forEach((order => {
+            if (Array.isArray(order.items))
+            {
+                const newItems = {}
+                order.items.forEach((item, index) => {
+                    if (item) {
+                    newItems[index] = item
+                    }
+                })
+                order.items = newItems
+            }
+            }))
+            setOrders(finalOrders)
+        } else {
+            setOrders([])
+            return [] // Return an empty array if snapshot does not exist
+        }
+    } catch (error) {
+        console.log(error)
+        return [] // Return an empty array in case of an error
+    }
+  }
+
+  const sortOrders = () => {
+    if (orderSort === 'total') {
+        let sortedOrders = null
+        if (ascending) 
+            sortedOrders = [...orders].sort((a, b) => a.amount - b.amount)
+        else
+            sortedOrders = [...orders].sort((a, b) => b.amount - a.amount)
+
+        setOrders(sortedOrders)
+    }
+    else if (orderSort === 'date') {
+        const sortedOrders = [...orders].sort((a, b) => {
+            const dateA = new Date(a.date)
+            const dateB = new Date(b.date)
+            if (ascending)
+                return dateA - dateB
+            else
+                return dateB - dateA
+        })
+        setOrders(sortedOrders)
+    }
   }
 
   const fetchCodes = async () => {
@@ -174,43 +238,6 @@ export const Admin = () => {
   }, [showProductSplash])
 
   useEffect(() => {
-    const fetchOrders = async () => {
-        try {
-            const ordersRef = ref(database, 'orders')
-            const snapshot = await get(ordersRef)
-            if (snapshot.exists()) {
-                const data = snapshot.val()
-                const orderNumbers = Object.keys(data)
-                const finalOrders = Object.values(data)
-                finalOrders.forEach((order, index) => {
-                    order.key = orderNumbers[index]
-                })
-                // Remove empty items in finalOrders (possibly weird firebase thing)
-                // Firebase makes the items into a list/array rather than a JSON object, this converts it back:
-                // Note that this only happens for SOME items, some are read as JSONs as intended, and thus don't need conversion.
-                finalOrders.forEach((order => {
-                if (Array.isArray(order.items))
-                {
-                    const newItems = {}
-                    order.items.forEach((item, index) => {
-                        if (item) {
-                        newItems[index] = item
-                        }
-                    })
-                    order.items = newItems
-                }
-                }))
-                setOrders(finalOrders)
-            } else {
-                setOrders([])
-                return [] // Return an empty array if snapshot does not exist
-            }
-        } catch (error) {
-            console.log(error)
-            return [] // Return an empty array in case of an error
-        }
-    }
-
     const fetchUsers = async () => {
         try {
             const userRef = ref(database, 'users')
@@ -238,6 +265,10 @@ export const Admin = () => {
     fetchProducts()
   }, [])
 
+  useEffect(() => {
+    sortOrders()
+  }, [orderSort, ascending])
+
   return (
     <div className={styles.adminPage}>
         {!authIsAdmin ? 
@@ -251,15 +282,22 @@ export const Admin = () => {
         </div>
         <div className={styles.top}>
             <div className={styles.users}>
-                <div className={styles.activeBttnBG}>
-                    <button className={activeOnly ? (`${styles.lightBttn} ${styles.lightBttnClcked}`) : (styles.lightBttn)} onClick={() => setActiveOnly(!activeOnly)}>Active Only</button>
-                </div>
                 {Object.keys(users).map((uid) => {
                     return <Userbox key={uid} uid={uid} data={users[uid]} selected={selected} selectUser={selectUser}/>
                 })}
             </div>
 
             <div className={styles.orders}>
+                <div className={styles.orderOptions}>
+                    <p>Sort by:</p>
+                    <select onChange={(e) => setOrderSort(e.target.value)}>
+                        <option value={null}></option>
+                        <option value={'date'}>Date</option>
+                        <option value={'total'}>Total</option>
+                    </select>
+                    <button className={styles.ascendBttn} onClick={() => setAscending(!ascending)}>{ascending ? 'Λ' : 'V'}</button>
+                    <button className={activeOnly ? (`${styles.lightBttn} ${styles.lightBttnClcked}`) : (styles.lightBttn)} onClick={() => setActiveOnly(!activeOnly)}>Active Only</button>
+                </div>
                 {orders.map((order) => {
                     if (activeOnly)
                     {
