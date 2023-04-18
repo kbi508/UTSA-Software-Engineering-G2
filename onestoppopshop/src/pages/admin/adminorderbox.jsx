d4bcf83d463e3abb253ebb557f66967667658751
Connor Acred <c.acred@yahoo.com>
2023-04-17 19:38:14 -0500

    Added items to admin order box.

---
+++ onestoppopshop/src/pages/admin/adminorderbox.jsx
@@ -0,0 +1,22 @@
+import React, { useContext } from 'react'
+import styles from './adminorderbox.module.css'
+import { ShopContext } from '../../context/shop-context'
+
+export const Adminorderbox = (props) => {
+  const { products } = useContext(ShopContext)
+
+  return (
+    <div className={styles.box}>
+        <p id={styles.orderNum}>{props.data.key}</p>
+        <p id={styles.total}>${Number(props.data.amount).toFixed(2)}</p>
+        <p id={styles.date}>{props.data.date}</p>
+        <div className={styles.items}>
+        {Object.keys(props.data.items).map((prodNum) => {
+            return (
+            <><span>{products[prodNum].name}</span> <span className={styles.price}>${Number(products[prodNum].price).toFixed(2)} x {props.data.items[prodNum]} = ${Number(products[prodNum].price*props.data.items[prodNum]).toFixed(2)}</span></>
+            )
+        })}
+        </div>
+    </div>
+  )
+}
