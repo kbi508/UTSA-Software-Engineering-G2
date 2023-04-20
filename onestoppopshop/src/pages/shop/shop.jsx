061619fee2aa94c008314f0ba7485ff42c1bf8a6
Connor Acred <c.acred@yahoo.com>
2023-04-19 20:02:09 -0500

    Implemented searchbar functionality.

--- onestoppopshop/src/pages/shop/shop.jsx
+++ onestoppopshop/src/pages/shop/shop.jsx
@@ -8,23 +8,38 @@ import { ShopContext } from '../../context/shop-context'
 export const Shop = () => {
   const { products, fetchProducts } = useContext(ShopContext)
   const [searchString, setSearchString] = useState('')
+  const [filteredProducts, setFilteredProducts] = useState([])
 
   useEffect(() => {
     fetchProducts()
+    
   }, [])
 
-  const searchProducts = (e) => {
-    setSearchString(e.target.value)
-  }
+  useEffect(() => {
+    setFilteredProducts(products)
+  }, [products])
+
+  // When the search string is updated:
+  useEffect(() => {
+    if (searchString)
+      setFilteredProducts(products.filter((product) => 
+        product.prod_description.toUpperCase().search(searchString.toUpperCase()) !== -1 ||
+        product.name.toUpperCase().search(searchString.toUpperCase()) !== -1  ||
+        product.hastags.some((tag) => (tag.toUpperCase().search(searchString.toUpperCase()) !== -1))
+      ))
+    else
+      setFilteredProducts(products) // If the string was removed, show all products now.
+  }, [searchString])
+
 
   return (
     <div className={styles.shop}>
       <div className={styles.searchsort}>
-        <input className={styles.search} type='text' placeholder='Search' value={searchString} onChange={(e) => searchProducts(e)} />
+        <input className={styles.search} type='text' placeholder='Search' value={searchString} onChange={(e) => setSearchString(e.target.value)} />
       </div>
       <div className={styles.shopContent}>
         <div className={styles.products}>
-          {products.map((product, index) => (<Product key={index} prodNum={index} data={product}/>))}
+          {filteredProducts.map((product, index) => (<Product key={index} prodNum={index} data={product}/>))}
         </div>
       </div>
       <Cart />
