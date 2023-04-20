64d6ff87a9098e92cc5b83a9cb8d19a1b49f9ed5
Connor Acred <c.acred@yahoo.com>
2023-04-19 19:14:20 -0500

    Added initialze search bar.

--- onestoppopshop/src/pages/shop/shop.jsx
+++ onestoppopshop/src/pages/shop/shop.jsx
@@ -7,13 +7,21 @@ import { ShopContext } from '../../context/shop-context'
 
 export const Shop = () => {
   const { products, fetchProducts } = useContext(ShopContext)
+  const [searchString, setSearchString] = useState('')
 
   useEffect(() => {
     fetchProducts()
   }, [])
 
+  const searchProducts = (e) => {
+
+  }
+
   return (
     <div className={styles.shop}>
+      <div className={styles.searchsort}>
+        <input type='text' placeholder='Search' value={searchString} onChange={(e) => searchProducts(e.target.value)} />
+      </div>
       <div className={styles.shopContent}>
         <div className={styles.products}>
           {products.map((product, index) => (<Product key={index} prodNum={index} data={product}/>))}
