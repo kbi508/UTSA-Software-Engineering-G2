a92d5c4f88e39ec637d13ef436de7d05bc8d0ae2
Connor Acred <c.acred@yahoo.com>
2023-04-19 20:58:41 -0500

    Added sorting buttons with style, added loading screen to shop.

--- onestoppopshop/src/pages/shop/shop.jsx
+++ onestoppopshop/src/pages/shop/shop.jsx
@@ -9,10 +9,12 @@ export const Shop = () => {
   const { products, fetchProducts } = useContext(ShopContext)
   const [searchString, setSearchString] = useState('')
   const [filteredProducts, setFilteredProducts] = useState([])
+  const [productSort, setProductSort] = useState(null)
+  const [ascending, setAscending] = useState(false)
+
 
   useEffect(() => {
     fetchProducts()
-    
   }, [])
 
   useEffect(() => {
@@ -31,16 +33,48 @@ export const Shop = () => {
       setFilteredProducts(products) // If the string was removed, show all products now.
   }, [searchString])
 
+  useEffect(() => {
+    if (productSort === 'price') {
+      let sortedProducts = null
+      if (ascending) 
+          sortedProducts = [...filteredProducts].sort((a, b) => a.price - b.price)
+      else
+          sortedProducts = [...filteredProducts].sort((a, b) => b.price - a.price)
+
+      setFilteredProducts(sortedProducts)
+    }
+    else if (productSort === 'quantity') {
+        const sortedProducts = [...filteredProducts].sort((a, b) => {
+          if (ascending)
+            return a.quantity - b.quantity
+          else
+            return b.quantity - a.quantity
+        })
+        console.log(sortedProducts)
+        setFilteredProducts(sortedProducts)
+    }
+  }, [productSort, ascending])
+
 
   return (
     <div className={styles.shop}>
       <div className={styles.searchsort}>
-        <input className={styles.search} type='text' placeholder='Search' value={searchString} onChange={(e) => setSearchString(e.target.value)} />
+        <div style={{whiteSpace: 'nowrap'}}>
+          <input className={styles.search} type='text' placeholder='Search' value={searchString} onChange={(e) => setSearchString(e.target.value)} />
+          <select className={styles.sort} onChange={(e) => setProductSort(e.target.value)}>
+              <option value={null}></option>
+              <option value={'quantity'}>Availability</option>
+              <option value={'price'}>Price</option>
+          </select>
+          <button className={styles.ascendBttn} onClick={() => setAscending(!ascending)}>{ascending ? 'Low...High' : 'High...Low'}</button>
+        </div>
       </div>
       <div className={styles.shopContent}>
-        <div className={styles.products}>
+      {filteredProducts.length === 0 ? (<p className={styles.loading}>Loading...</p>) : 
+        (<><div className={styles.products}>
           {filteredProducts.map((product, index) => (<Product key={index} prodNum={index} data={product}/>))}
-        </div>
+        </div></>)
+      }
       </div>
       <Cart />
     </div>
