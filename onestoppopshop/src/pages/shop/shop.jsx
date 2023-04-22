a4278bc88e09ad81a5ed41e9438cf984a378db98
kbi508 <99996743+kbi508@users.noreply.github.com>
2023-04-22 11:05:28 -0500

    Added products splash, and added out of stock functionality

--- onestoppopshop/src/pages/shop/shop.jsx
+++ onestoppopshop/src/pages/shop/shop.jsx
@@ -3,24 +3,74 @@ import { Product } from './product'
 import { Cart } from '../../components/cart'
 import styles from './shop.module.css'
 import { ShopContext } from '../../context/shop-context'
-import { shopProductSplash } from './shopProductSplash'
+import { ShopProductSplash } from './shopProductSplash'
 
 
 export const Shop = () => {
   const { products, fetchProducts } = useContext(ShopContext)
+  const [desc, setDesc] = useState('')
+  const [price, setPrice] = useState('')
+  const [name, setName] = useState('')
+  const [img, setImg] = useState('')
+  const [weightUnit, setWeightUnit] = useState('')
+  const [weight, setWeight] = useState('')
+  const [quantity, setQuantity] = useState('')
+  const [sale, setSale] = useState(false)
+  const [discount, setDiscount] = useState('')
+  const [tags, setTags] = useState([])
+  const [curProdNum, setCurProdNum] = useState(null)
+  const [showProductSplash, setShowProductSplash] = useState(false)
+  const productVals = {desc, price, name, img, weightUnit, weight, quantity, sale, discount, tags, setTags, setSale, setDiscount, setDesc, setPrice, setName, setImg,setWeightUnit, setWeight, setQuantity}
+
 
   useEffect(() => {
     fetchProducts()
   }, [])
 
+  useEffect(() => {
+    if (!showProductSplash)
+    {
+        setDesc('')
+        setPrice('')
+        setName('')
+        setImg('')
+        setWeightUnit('')
+        setWeight('')
+        setQuantity('')
+        setSale(false)
+        setDiscount('')
+        setTags([])
+        setCurProdNum(null)
+    }
+  }, [showProductSplash])
+
+  const productScreen = (productNum=null) => {
+    console.log("Attempting" + productNum)
+    if (productNum) { // If this is an edit call, pass in the data:
+        setDesc(products[productNum].prod_description)
+        setPrice(products[productNum].price)
+        setName(products[productNum].name)
+        setImg(products[productNum].product_Image)
+        setWeightUnit(products[productNum].weight_Type)
+        setWeight(products[productNum].weight_Amount)
+        setQuantity(products[productNum].quantity)
+        setSale(products[productNum].onsale)
+        setDiscount(Number(products[productNum].salepercent * 100))
+        setTags(products[productNum].hastags)
+        setCurProdNum(productNum)
+        setShowProductSplash(true)
+    }
+  }
+
   return (
     <div className={styles.shop}>
       <div className={styles.shopContent}>
         <div className={styles.products}>
-          {products.map((product, index) => (<Product key={index} prodNum={index} data={product}/>))}
+          {products.map((product, index) => (<Product key={index} prodNum={index} data={product} productScreen={productScreen}/>))}
         </div>
       </div>
       <Cart />
+      {showProductSplash && <ShopProductSplash data={productVals} close={setShowProductSplash}/>}
     </div>
   )
 }
--- onestoppopshop/src/pages/shop/shopProductSplash.jsx
+++ onestoppopshop/src/pages/shop/shopProductSplash.jsx
@@ -1,33 +1,36 @@
 import React, { useState } from 'react'
 import styles from './shopProductSplash.module.css'
-//import { Tag } from './tag'
-export const shopProductSplash = (props) => {
-    const { desc, price, name, img, weightUnit, weight, quantity, sale, discount, tags} = props.data
+import { Tag } from './tag'
+export const ShopProductSplash = (props) => {
+const { desc, price, name, img, weightUnit, weight, quantity, sale, discount, tags} = props.data
 
-    return (
-        <div className={styles.backdrop}>
-            <div className={styles.productSplash}>
-                <div className={styles.splashHeader}>
+return (
+    <div className={styles.backdrop}>
+        <div className={styles.productSplash}>
+            <div className={styles.splashHeader}>
+                <button className={styles.lightBttn} onClick={() => props.close()}>X</button>
+                <img src={img} alt='Product'/>
+                <div className={styles.prodName}>
                     <p>{name}</p>
-                    <button className={styles.lightBttn} onClick={() => props.close()}>X</button>
-                    <img src={img} alt='Product'/>
-                    <div className={styles.prodDesc}>
-                        <p>{price}</p>
-                        <p>{desc}</p>
-                        <p>{weight}</p>
-                        <p>{weightUnit}</p>
-                        <p>{sale}</p>
-                        <p>{discount}</p>
-                        <div className={styles.tags}>
-                            {tags.map(tag)}
-                        </div>
-
+                </div>
+                <div className={styles.prodDesc}>
+                    <div><p>{desc}</p></div>
+                    <div><p>Price: ${price}</p></div>
+                    
+                    <div><p>{sale}</p></div>
+                    <div> <p>{discount}</p></div>
+                    <div className={styles.tags}>
+                    {tags.map((tag) => <Tag text={tag} />)}
                     </div>
 
                 </div>
 
             </div>
-        </div>    
-        
-    )    
+
+        </div>
+    </div>    
+
+    
+    
+)    
 }
