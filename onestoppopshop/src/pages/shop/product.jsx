10e979372621a5d671e6621dc0fb6f567b2cdec7
Connor Acred <c.acred@yahoo.com>
2023-04-22 21:10:02 -0500

    Added sale badge to products.

--- onestoppopshop/src/pages/shop/product.jsx
+++ onestoppopshop/src/pages/shop/product.jsx
@@ -1,21 +1,21 @@
-import React, { useContext, useEffect, useState } from 'react'
+import React, { useContext } from 'react'
 import {ShopContext} from '../../context/shop-context'
 import styles from './product.module.css'
 
 export const Product = (props) => {
-  const {name, price, product_Image, prod_description, hastags, quantity} = props.data
+  const {name, price, product_Image, prod_description, hastags, quantity, salepercent, onsale} = props.data
   const { addToCart, cartItems } = useContext(ShopContext)
 
   return (
     <div className={styles.product} >
+        {onsale && <p className={styles.sale}>Sale!</p>}
         <img src={product_Image}  onClick={() => props.productScreen(props.prodNum)} alt='Product'/>
         <div className={styles.prodDesc}>
             <p><b>{name}</b></p>
-            <p>${price.toFixed(2)}</p>
-        </div>
-        
+            <p>Quantity: {quantity}</p>
+            <p>${onsale ? (price * (1-(salepercent))).toFixed(2) : price} {onsale && '(' + salepercent*100 + '% off)'}</p>
+        </div>        
         <button className={styles.addToCartBttn} onClick={() => addToCart(props.prodNum)} disabled={quantity === 0}>
-          {/* Add To Cart {cartItems[props.prodNum] > 0 && <>({cartItems[props.prodNum]})</>} {quantity === 0 ? '(Out of Stock)' : ''} */}
           {quantity === 0 ? 'Out of Stock' : `Add To Cart ${cartItems[props.prodNum] > 0 ? `(${cartItems[props.prodNum]})` : ('')}`}
         </button>
     </div>
--- onestoppopshop/src/pages/shop/shopProductSplash.jsx
+++ onestoppopshop/src/pages/shop/shopProductSplash.jsx
@@ -1,4 +1,4 @@
-import React, { useContext, useState } from 'react'
+import React, { useContext } from 'react'
 import styles from './shopProductSplash.module.css'
 import { Tag } from './tag'
 import { ShopContext } from '../../context/shop-context'
